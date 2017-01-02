76d6e3616b6dcaee1566ed0b4f70e7f5945b130b
Prashanth Sampath Kumar <pskumar@fb.com>
2017-01-01 22:02:44 -0800

    Adding like and share buttons

--- app/Components/Wishes.js
+++ app/Components/Wishes.js
@@ -7,7 +7,7 @@
 'use strict';
 
 const React = require('react');
-import FacebookProvider, { Comments } from 'react-facebook';
+import FacebookProvider, { Comments, Like, Share, ShareButton } from 'react-facebook';
 
 type State = {
 };
@@ -36,12 +36,15 @@ class Wishes extends React.Component<void, State, Props> {
 
   componentWillMount() {
     this.renderedComponent =
-        <div>
-          <p className='hideMe'>May take few seconds to load...</p>
-          <FacebookProvider appID="1815795118693626">
-            <Comments href="https://prash88.github.io/my-wedding/" />
-          </FacebookProvider>
-        </div>;
+      <div>
+        <p className='hideMe'>May take few seconds to load...</p>
+        <FacebookProvider appID="1815795118693626">
+          <Like href="https://prash88.github.io/my-wedding/" colorScheme="dark" showFaces share />
+        </FacebookProvider>
+        <FacebookProvider appID="1815795118693626">
+          <Comments href="https://prash88.github.io/my-wedding/" />
+        </FacebookProvider>
+      </div>;
   }
 
   render () {
