acbf0b82dde55f70bd1b51c8622823c57d00a61e
Prashanth Sampath Kumar <pskumar@fb.com>
2017-01-01 21:17:51 -0800

    Adding images

--- app/Components/Invitation.js
+++ app/Components/Invitation.js
@@ -44,23 +44,27 @@ class Invitation extends React.Component<void, State, Props> {
           <div className ="info">
             <h2>Together with our parents, we cordially invite you and your family's gracious presence at the wedding & reception of</h2>
             <h1>Prashanth & Sweta</h1>
-              <p className= "line">___________________________</p>
-              <h2>Reception</h2>
-              <p>On Wednesday, Feb 8, 2017</p>
-              <p>After 6:30 PM</p>
-              <p>At Vijaya Mahal, 33 Pondi bazar, Thiyagaraya Nagar, Chennai - 11</p>
-              <br />
-              <h2>Wedding</h2>
-              <p>On Thursday, Feb 9, 2017</p>
-              <p>Between 7:30am - 8.30 am</p>
-              <p>At Vijaya Mahal, 33 Pondi bazar, Thiyagaraya Nagar, Chennai - 11</p>
-              <br />
-              <h2>Reception</h2>
-              <p>On Sunday, Feb 12, 2017</p>
-              <p>After 6:30 PM</p>
-              <p>At Akshaya Hall, Amutha Bhavan, Sampath Nagar, Erode - 11</p>
-              <p className= "line">____________________________</p>
-              <h2>With best compliments from near and dear</h2>
+            <div className="clover">
+              <img src={require('../Assets/prashanth_cropped.jpg')} className="a" />
+              <img src={require('../Assets/sweta_cropped.jpg')} className="b" />
+            </div>
+            <p className= "line">___________________________</p>
+            <h2>Reception</h2>
+            <p>On Wednesday, Feb 8, 2017</p>
+            <p>After 6:30 PM</p>
+            <p>At Vijaya Mahal, 33 Pondi bazar, Thiyagaraya Nagar, Chennai - 11</p>
+            <br />
+            <h2>Wedding</h2>
+            <p>On Thursday, Feb 9, 2017</p>
+            <p>Between 7:30am - 8.30 am</p>
+            <p>At Vijaya Mahal, 33 Pondi bazar, Thiyagaraya Nagar, Chennai - 11</p>
+            <br />
+            <h2>Reception</h2>
+            <p>On Sunday, Feb 12, 2017</p>
+            <p>After 6:30 PM</p>
+            <p>At Akshaya Hall, Amutha Bhavan, Sampath Nagar, Erode - 11</p>
+            <p className= "line">____________________________</p>
+            <h2>With best compliments from near and dear</h2>
           </div>
         </div>
       </div>
